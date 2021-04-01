7f354180f84647e4fa5b520d23c816925e0b22bb
Timothy Brown <tabrownies@gmail.com>
2021-04-01 17:06:39 -0600

    I think roman to alpha totally works. I need to brute force test it, but I think it is fine.

--- romanToAlpha.js
+++ romanToAlpha.js
@@ -3,11 +3,33 @@
 //a function to evalutate the string
 function evalutateRomanNum(romanNumString){
     //I'll start with a basic aproach
-    //make it to uppercase
     let sum = 0;
+    //change to uppercase
     romanNumString = romanNumString.toUpperCase();
-    for(romanChar of romanNumString){
-        sum+=convertRomanCharToValue(romanChar);
+    let subtractor = 0;
+    for(let index = 0; index < romanNumString.length; ++index){
+        //this ensures that the subtraction comparsions don't fail at the end of the string
+        if(index + 1 == romanNumString.length){
+            sum+=convertRomanCharToValue(romanNumString[index])-subtractor;
+            subtractor=0;
+        }
+        else{
+            if(convertRomanCharToValue(romanNumString[index])<convertRomanCharToValue(romanNumString[index+1])){
+                //this error is thown if a symbol representing 10^x precedes a symbol larger than 10^(x+1)
+                //I can't tell by the instructions if it is supposed to be thrown by VX = 5 because V=5. From what I read I think that VX is legal, but I'll check back
+                if(convertRomanCharToValue(romanNumString[index])<convertRomanCharToValue(romanNumString[index+1])/10){
+                    
+                    let error = `${romanNumString[index+1]} cannot be preceded by ${romanNumString[index]}`;
+                    throw error;
+                }
+                subtractor = convertRomanCharToValue(romanNumString[index]);
+            }
+            else{
+                sum+=convertRomanCharToValue(romanNumString[index])-subtractor;
+                subtractor=0;
+            }
+        }
+        
     }
     return sum;
 }
@@ -43,7 +65,13 @@ function convertRomanCharToValue(romanChar){
 }
 //dummy calls
 //alert(convertRomanCharToValue('I'));
-alert(evalutateRomanNum("Iv"));
+try{
+    
+    alert(evalutateRomanNum('cc'));
+}catch(error){
+    alert(error);
+}
+
 
 
 
