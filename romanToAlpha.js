2f4c832e3ce7a7d672a49131ba333ca461501242
Timothy Brown <tabrownies@gmail.com>
2021-04-01 16:49:11 -0600

    A basic funciton works. It is time to adjust it to deal with subtractive values.

--- romanToAlpha.js
+++ romanToAlpha.js
@@ -1,3 +1,49 @@
 //read in roman numeral, eventually replace this with an HTML5 interface
-let romanNumString = prompt("Enter a roman numeral");
+//let romanNumString = prompt("Enter a roman numeral");
+//a function to evalutate the string
+function evalutateRomanNum(romanNumString){
+    //I'll start with a basic aproach
+    //make it to uppercase
+    let sum = 0;
+    romanNumString = romanNumString.toUpperCase();
+    for(romanChar of romanNumString){
+        sum+=convertRomanCharToValue(romanChar);
+    }
+    return sum;
+}
+function convertRomanCharToValue(romanChar){
+    let value = 0;
+    switch(romanChar){
+        case 'I':
+            value = 1;
+            break;
+        case 'V':
+            value = 5;
+            break;
+        case 'X':
+            value = 10;
+            break;
+        case 'L':
+            value = 50;
+            break;
+        case 'C':
+            value = 100;
+            break;
+        case 'D':
+            value = 500;
+            break;
+        case 'M':
+            value = 1000;
+            break;
+        default:
+            throw "INVALID ROMAN NUMERAL";
+            break;
+    }
+    return value;
+}
+//dummy calls
+//alert(convertRomanCharToValue('I'));
+alert(evalutateRomanNum("Iv"));
+
+
 
