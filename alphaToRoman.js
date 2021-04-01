22dc2e9324fb9ad87c25b624557d5d17f999e78c
Timothy Brown <tabrownies@gmail.com>
2021-04-01 17:40:11 -0600

    All working and formated

---
+++ alphaToRoman.js
@@ -0,0 +1,99 @@
+//read in an arabic numeral
+//let arabicNum = prompt("Enter an arabic numeral");
+
+function evaluateArabicNum(arabicNum) {
+    //starter place for roman numeral
+    let romanNum = '';
+    /*
+        Algorithm:
+            Loop until arabicNum==0;
+            With each loop figure out what roman numerals to append to romanNum and subtract their value from arabicNum
+    */
+    if (arabicNum == 0) {
+        throw "ENTER A VALUE GREATER THAN 0";
+    } else {
+        while (arabicNum > 0) {
+            //I think I can simplify this whole process
+            if (arabicNum >= 1000) {
+                //I wonder if I can simplify this to simply take advantage of the while loop.
+                let numMs = (arabicNum - (arabicNum % 1000)) / 1000;
+                
+                for (let index = 0; index < numMs; ++index) {
+                    romanNum += 'M';
+                    arabicNum -= 1000;
+                }
+            } else if (arabicNum >= 100) {
+                if (arabicNum >= 900) {
+                    romanNum += 'CM';
+                    arabicNum -= 900;
+                } else if (arabicNum >= 500) {
+                    romanNum += 'D';
+                    arabicNum -= 500;
+                } else if (arabicNum >= 400) {
+                    romanNum+= 'CD';
+                    arabicNum-=400;
+                }
+                else if(arabicNum >=100){
+                    romanNum+='C';
+                    arabicNum-=100;
+                }
+            } else if(arabicNum>=10){
+                if (arabicNum >= 90) {
+                    romanNum += 'XC';
+                    arabicNum -= 90;
+                } else if (arabicNum >= 50) {
+                    romanNum += 'L';
+                    arabicNum -= 50;
+                } else if (arabicNum >= 40) {
+                    romanNum+= 'XL';
+                    arabicNum-=40;
+                }
+                else if(arabicNum >=10){
+                    romanNum+='X';
+                    arabicNum-=10;
+                }   
+            } else if(arabicNum>=1){
+                if (arabicNum >= 9) {
+                    romanNum += 'IX';
+                    arabicNum -= 9;
+                } else if (arabicNum >= 5) {
+                    romanNum += 'V';
+                    arabicNum -= 5;
+                } else if (arabicNum >= 4) {
+                    romanNum+= 'IV';
+                    arabicNum-=4;
+                }
+                else if(arabicNum >=1){
+                    romanNum+='I';
+                    arabicNum-=1;
+                }
+            }
+        }
+    }
+    return romanNum;
+
+
+
+
+
+
+    //this separates the arabic number into values easier to parse into a roman numeral
+    /*let mDigit = arabicNum-(arabicNum % 1000);
+    let cDigit = arabicNum%1000;
+    //let dDigit 
+    //let lDigit = 
+    let xDigit = arabicNum%100;
+    //let vDigit
+    let iDigit = arabicNum%10;
+    alert(mDigit);
+    alert(cDigit);
+    alert(xDigit);
+    alert(iDigit);*/
+}
+
+try {
+    alert(evaluateArabicNum(1991));
+    alert(evaluateArabicNum(1544));
+} catch (error) {
+    alert(error);
+}
--- romanToAlpha.js
+++ romanToAlpha.js
@@ -1,41 +1,40 @@
 //read in roman numeral, eventually replace this with an HTML5 interface
 //let romanNumString = prompt("Enter a roman numeral");
 //a function to evalutate the string
-function evalutateRomanNum(romanNumString){
+function evalutateRomanNum(romanNumString) {
     //I'll start with a basic aproach
     let sum = 0;
     //change to uppercase
     romanNumString = romanNumString.toUpperCase();
     let subtractor = 0;
-    for(let index = 0; index < romanNumString.length; ++index){
+    for (let index = 0; index < romanNumString.length; ++index) {
         //this ensures that the subtraction comparsions don't fail at the end of the string
-        if(index + 1 == romanNumString.length){
-            sum+=convertRomanCharToValue(romanNumString[index])-subtractor;
-            subtractor=0;
-        }
-        else{
-            if(convertRomanCharToValue(romanNumString[index])<convertRomanCharToValue(romanNumString[index+1])){
+        if (index + 1 == romanNumString.length) {
+            sum += convertRomanCharToValue(romanNumString[index]) - subtractor;
+            subtractor = 0;
+        } else {
+            if (convertRomanCharToValue(romanNumString[index]) < convertRomanCharToValue(romanNumString[index + 1])) {
                 //this error is thown if a symbol representing 10^x precedes a symbol larger than 10^(x+1)
                 //I can't tell by the instructions if it is supposed to be thrown by VX = 5 because V=5. From what I read I think that VX is legal, but I'll check back
-                if(convertRomanCharToValue(romanNumString[index])<convertRomanCharToValue(romanNumString[index+1])/10){
-                    
+                if (convertRomanCharToValue(romanNumString[index]) < convertRomanCharToValue(romanNumString[index + 1]) / 10) {
+
                     let error = `${romanNumString[index+1]} cannot be preceded by ${romanNumString[index]}`;
                     throw error;
                 }
                 subtractor = convertRomanCharToValue(romanNumString[index]);
-            }
-            else{
-                sum+=convertRomanCharToValue(romanNumString[index])-subtractor;
-                subtractor=0;
+            } else {
+                sum += convertRomanCharToValue(romanNumString[index]) - subtractor;
+                subtractor = 0;
             }
         }
-        
+
     }
     return sum;
 }
-function convertRomanCharToValue(romanChar){
+
+function convertRomanCharToValue(romanChar) {
     let value = 0;
-    switch(romanChar){
+    switch (romanChar) {
         case 'I':
             value = 1;
             break;
@@ -65,13 +64,9 @@ function convertRomanCharToValue(romanChar){
 }
 //dummy calls
 //alert(convertRomanCharToValue('I'));
-try{
-    
+try {
+
     alert(evalutateRomanNum('cc'));
-}catch(error){
+} catch (error) {
     alert(error);
 }
-
-
-
-
