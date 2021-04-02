4cd47c85290302137a39cf16c4e3919697702442
Timothy Brown <tabrownies@gmail.com>
2021-04-01 18:12:24 -0600

    I got rid of the last logic bug

--- romanToAlpha.js
+++ romanToAlpha.js
@@ -8,21 +8,21 @@ function evalutateRomanNum(romanNumString) {
     romanNumString = romanNumString.toUpperCase();
     let subtractor = 0;
     for (let index = 0; index < romanNumString.length; ++index) {
-        //this ensures that the subtraction comparsions don't fail at the end of the string
+        //this ensures that the subtraction comparsions don't throw errors at the end of the string
         if (index + 1 == romanNumString.length) {
             sum += convertRomanCharToValue(romanNumString[index]) - subtractor;
             subtractor = 0;
         } else {
+            //revisit this logic
             if (convertRomanCharToValue(romanNumString[index]) < convertRomanCharToValue(romanNumString[index + 1])) {
                 //this error is thown if a symbol representing 10^x precedes a symbol larger than 10^(x+1)
                 //I can't tell by the instructions if an error is supposed to be thrown by VX = 5 because V=5. From what I read I think that VX is illegal and should throw an error
-                if (convertRomanCharToValue(romanNumString[index]) < convertRomanCharToValue(romanNumString[index + 1]) / 10) {
-
+                if (!canPrecede(romanNumString[index], romanNumString[index + 1])) {
                     let error = `${romanNumString[index+1]} cannot be preceded by ${romanNumString[index]}`;
                     throw error;
                 }
                 //this error is thrown if two subracting pre-digits are present. The second condition is present because this error checking won't work if index is pointing to the first charecter in the string.
-                if(index!=0&&convertRomanCharToValue(romanNumString[index])>=convertRomanCharToValue(romanNumString[index-1])){
+                if (index != 0 && convertRomanCharToValue(romanNumString[index]) >= convertRomanCharToValue(romanNumString[index - 1])) {
                     throw "SUBTRACTIVE NOTATION HAS MORE THAN TWO SUBTRACTORS"
                 }
                 subtractor = convertRomanCharToValue(romanNumString[index]);
@@ -66,11 +66,23 @@ function convertRomanCharToValue(romanChar) {
     }
     return value;
 }
+
+function canPrecede(firstRomanChar, secondRomanChar) {
+    //This returns true because if the first character has a larger value than the second, it can precede the second
+    if (convertRomanCharToValue(firstRomanChar) > convertRomanCharToValue(secondRomanChar)) {
+        return true;
+    }
+    //These return false because if, for example, V precedes X (VX) has the same value as V, which is ambiguous
+    if (firstRomanChar == 'V' || firstRomanChar == 'L' || firstRomanChar == 'D') {
+        return false;
+    }
+    return convertRomanCharToValue(firstRomanChar) * 10 > convertRomanCharToValue(secondRomanChar) / 10
+}
 //dummy calls
 //alert(convertRomanCharToValue('I'));
 try {
-
-    alert(evalutateRomanNum('ivx'));
+    //alert(evalutateRomanNum('lc'));
+    alert(canPrecede("C", "M"));
 } catch (error) {
     alert(error);
 }
